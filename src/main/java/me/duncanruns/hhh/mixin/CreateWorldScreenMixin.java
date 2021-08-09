0b1928e9da246f656e91475c473980315912bb74
DuncanRL <59705125+DuncanRuns@users.noreply.github.com>
2021-08-09 18:56:25 -0300

    1.14 poggies

--- src/main/java/me/duncanruns/hhh/mixin/CreateWorldScreenMixin.java
+++ src/main/java/me/duncanruns/hhh/mixin/CreateWorldScreenMixin.java
@@ -1,7 +1,6 @@
 package me.duncanruns.hhh.mixin;
 
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
-import net.minecraft.world.Difficulty;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,10 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class CreateWorldScreenMixin {
 
     @Shadow
-    private boolean hardcore;
+    private boolean field_3178;
 
     @Inject(method = "createLevel", at = @At("HEAD"))
     private void forceHardcoreMixin(CallbackInfo info) {
-        hardcore = true;
+        field_3178 = true;
     }
 }
